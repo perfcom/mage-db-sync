853179808b33d0fe82505ea3b399e695e345ce52
Louis de Looze <louis.delooze@gmail.com>
2024-12-06 14:13:45 +1100

    Make database import naming consistent

--- dist/tasks/importTask.js
+++ dist/tasks/importTask.js
@@ -12,8 +12,15 @@ class ImportTask {
         // Add tasks
         this.addTasks = (list, config) => tslib_1.__awaiter(this, void 0, void 0, function* () {
             list.add({
-                title: 'Import Magento database to localhost',
-                task: (ctx, task) => task.newListr(this.importTasks)
+                title: "Import Magento database to localhost",
+                task: (ctx, task) => task.newListr(this.importTasks),
+            });
+            this.importTasks.push({
+                title: "Modify active database",
+                task: () => tslib_1.__awaiter(this, void 0, void 0, function* () {
+                    let replaceCommand = `ddev php -r "\\$config = include('app/etc/env.php'); \\$config['db']['connection']['default']['dbname'] = '${config.serverVariables.databaseName}'; file_put_contents('app/etc/env.php', '<?php return ' . var_export(\\$config, true) . ';');"`;
+                    yield (0, console_1.localhostMagentoRootExec)(replaceCommand, config);
+                }),
             });
             let importTitle = "Importing database";
             if (config.settings.isDdevActive) {
@@ -24,12 +31,12 @@ class ImportTask {
                 task: () => tslib_1.__awaiter(this, void 0, void 0, function* () {
                     if (config.settings.isDdevActive) {
                         let mysqlCommand1 = `ddev mysql -uroot -proot -hdb -e "CREATE DATABASE IF NOT EXISTS ${config.serverVariables.databaseName};"""`;
-                        let mysqlCommand2 = `ddev mysql -uroot -proot -hdb -e "GRANT ALL PRIVILEGES ON *.* TO 'db'@'localhost';"""`;
-                        let mysqlCommand3 = `ddev mysql -uroot -proot -hdb -e "GRANT ALL PRIVILEGES ON *.* TO 'db'@'%';"""`;
+                        let mysqlCommand2 = `ddev mysql -uroot -proot -hdb -e "GRANT ALL PRIVILEGES ON *.* TO ${config.serverVariables.databaseName}@'localhost';"""`;
+                        let mysqlCommand3 = `ddev mysql -uroot -proot -hdb -e "GRANT ALL PRIVILEGES ON *.* TO ${config.serverVariables.databaseName}@'%';"""`;
                         yield (0, console_1.localhostMagentoRootExec)(mysqlCommand1, config, true);
                         yield (0, console_1.localhostMagentoRootExec)(mysqlCommand2, config, true);
                         yield (0, console_1.localhostMagentoRootExec)(mysqlCommand3, config, true);
-                        yield (0, console_1.localhostMagentoRootExec)(`ddev import-db --src=${config.serverVariables.databaseName}.sql`, config);
+                        yield (0, console_1.localhostMagentoRootExec)(`ddev import-db --database=${config.serverVariables.databaseName} --src=${config.serverVariables.databaseName}.sql`, config);
                     }
                     else {
                         // Create database
@@ -39,14 +46,14 @@ class ImportTask {
                         // Add default admin authorization rules (Fix for missing auth roles)
                         yield (0, console_1.localhostMagentoRootExec)(`${config.settings.magerun2CommandLocal} db:add-default-authorization-entries -q`, config);
                     }
-                })
+                }),
             });
             this.importTasks.push({
-                title: 'Cleaning up',
+                title: "Cleaning up",
                 task: () => tslib_1.__awaiter(this, void 0, void 0, function* () {
                     // Remove local SQL file
-                    yield (0, console_1.localhostMagentoRootExec)('rm ' + config.serverVariables.databaseName + '.sql', config);
-                })
+                    yield (0, console_1.localhostMagentoRootExec)("rm " + config.serverVariables.databaseName + ".sql", config);
+                }),
             });
         });
     }
--- src/tasks/importTask.ts
+++ src/tasks/importTask.ts
@@ -1,65 +1,80 @@
-import {localhostMagentoRootExec, success} from '../utils/console';
-import { Listr } from 'listr2';
+import * as fs from "fs";
+import { localhostMagentoRootExec, success } from "../utils/console";
+import { Listr } from "listr2";
 
 class ImportTask {
-    private importTasks = [];
+  private importTasks = [];
 
-    configure = async (list: any, config: any) => {
-        await this.addTasks(list, config);
-        return list;
+  configure = async (list: any, config: any) => {
+    await this.addTasks(list, config);
+    return list;
+  };
+
+  // Add tasks
+  addTasks = async (list: any, config: any) => {
+    list.add({
+      title: "Import Magento database to localhost",
+      task: (ctx: any, task: any): Listr => task.newListr(this.importTasks),
+    });
+
+    this.importTasks.push({
+      title: "Modify active database",
+      task: async (): Promise<void> => {
+        let replaceCommand = `ddev php -r "\\$config = include('app/etc/env.php'); \\$config['db']['connection']['default']['dbname'] = '${config.serverVariables.databaseName}'; file_put_contents('app/etc/env.php', '<?php return ' . var_export(\\$config, true) . ';');"`;
+        await localhostMagentoRootExec(replaceCommand, config);
+      },
+    });
+
+    let importTitle = "Importing database";
+    if (config.settings.isDdevActive) {
+      importTitle = "Importing database (DDEV)";
     }
 
-    // Add tasks
-    addTasks = async (list: any, config: any) => {
-        list.add(
-            {
-                title: 'Import Magento database to localhost',
-                task: (ctx: any, task: any): Listr =>
-                task.newListr(
-                    this.importTasks
-                )
-            }
-        )
-
-        let importTitle = "Importing database";
+    this.importTasks.push({
+      title: importTitle,
+      task: async (): Promise<void> => {
         if (config.settings.isDdevActive) {
-            importTitle = "Importing database (DDEV)";
+          let mysqlCommand1 = `ddev mysql -uroot -proot -hdb -e "CREATE DATABASE IF NOT EXISTS ${config.serverVariables.databaseName};"""`;
+          let mysqlCommand2 = `ddev mysql -uroot -proot -hdb -e "GRANT ALL PRIVILEGES ON *.* TO ${config.serverVariables.databaseName}@'localhost';"""`;
+          let mysqlCommand3 = `ddev mysql -uroot -proot -hdb -e "GRANT ALL PRIVILEGES ON *.* TO ${config.serverVariables.databaseName}@'%';"""`;
+          await localhostMagentoRootExec(mysqlCommand1, config, true);
+          await localhostMagentoRootExec(mysqlCommand2, config, true);
+          await localhostMagentoRootExec(mysqlCommand3, config, true);
+          await localhostMagentoRootExec(
+            `ddev import-db --database=${config.serverVariables.databaseName} --src=${config.serverVariables.databaseName}.sql`,
+            config,
+          );
+        } else {
+          // Create database
+          await localhostMagentoRootExec(
+            `${config.settings.magerun2CommandLocal} db:create -q`,
+            config,
+          );
+          // Import SQL file to database
+          await localhostMagentoRootExec(
+            `${config.settings.magerun2CommandLocal} db:import ${config.serverVariables.databaseName}.sql --force --skip-authorization-entry-creation -q --drop`,
+            config,
+          );
+          // Add default admin authorization rules (Fix for missing auth roles)
+          await localhostMagentoRootExec(
+            `${config.settings.magerun2CommandLocal} db:add-default-authorization-entries -q`,
+            config,
+          );
         }
+      },
+    });
 
-        this.importTasks.push(
-            {
-                title: importTitle,
-                task: async (): Promise<void> => {
-                    if (config.settings.isDdevActive) {
-                        let mysqlCommand1 = `ddev mysql -uroot -proot -hdb -e "CREATE DATABASE IF NOT EXISTS ${config.serverVariables.databaseName};"""`;
-                        let mysqlCommand2 = `ddev mysql -uroot -proot -hdb -e "GRANT ALL PRIVILEGES ON *.* TO 'db'@'localhost';"""`
-                        let mysqlCommand3 = `ddev mysql -uroot -proot -hdb -e "GRANT ALL PRIVILEGES ON *.* TO 'db'@'%';"""`
-                        await localhostMagentoRootExec(mysqlCommand1, config, true);
-                        await localhostMagentoRootExec(mysqlCommand2, config, true);
-                        await localhostMagentoRootExec(mysqlCommand3, config, true);
-                        await localhostMagentoRootExec(`ddev import-db --src=${config.serverVariables.databaseName}.sql`, config);
-                    } else {
-                        // Create database
-                        await localhostMagentoRootExec(`${config.settings.magerun2CommandLocal} db:create -q`, config);
-                        // Import SQL file to database
-                        await localhostMagentoRootExec(`${config.settings.magerun2CommandLocal} db:import ${config.serverVariables.databaseName}.sql --force --skip-authorization-entry-creation -q --drop`, config);
-                        // Add default admin authorization rules (Fix for missing auth roles)
-                        await localhostMagentoRootExec(`${config.settings.magerun2CommandLocal} db:add-default-authorization-entries -q`, config);
-                    }
-                }
-            }
+    this.importTasks.push({
+      title: "Cleaning up",
+      task: async (): Promise<void> => {
+        // Remove local SQL file
+        await localhostMagentoRootExec(
+          "rm " + config.serverVariables.databaseName + ".sql",
+          config,
         );
-
-        this.importTasks.push(
-            {
-                title: 'Cleaning up',
-                task: async (): Promise<void> => {
-                    // Remove local SQL file
-                    await localhostMagentoRootExec('rm ' + config.serverVariables.databaseName + '.sql', config);
-                }
-            }
-        );
-    }
+      },
+    });
+  };
 }
 
-export default ImportTask
+export default ImportTask;
